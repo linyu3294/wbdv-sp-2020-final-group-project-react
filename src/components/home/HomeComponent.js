07a1c61ca0a52b39c083620375449f17e3998960
bstarke <bstarke0225@gmail.com>
2020-04-14 21:08:08 -0400

    made the coolest logo ever

--- src/components/home/HomeComponent.js
+++ src/components/home/HomeComponent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {connect} from "react-redux";
 import {BrowserRouter as router, route} from "react-router-dom";
+import "./HomeComponent.css"
 
 class HomeComponent extends React.Component {
     constructor(props) {
@@ -17,19 +18,21 @@ class HomeComponent extends React.Component {
     render() {
         return (
             <div class="container">
-                <h1>Welcome To RentMatch.com</h1>
+                <div class="logo-box">
+                    <img src={require('./logo.png')} className="logo" alt="img"/>
+                </div>
                 <div class="row">
                     <div class="col-12">
                         <div className="jumbotron">
-                            <h1 className="display-4">Welcome To RentMatch.com!</h1>
+                            <h1 className="display-4">Welcome {this.state.username}</h1>
                             <p className="lead">Find your perfect home with a landlord you like</p>
                             <hr className="my-4"/>
                             <p>A large subset of the housing population is renters and landlords.
                                 Currently, there is no platform that connects potential renters to
                                 landlords and vice versa based on their shared values, living
-                                preferences, and character qualities. Essentially, our project
-                                creates a platform for renters to search for housing and for
-                                landlords based on their profile, their ratings, and any other
+                                preferences, and character qualities. RentMatch creates a platform
+                                for renters to search for housing and for landlords based on their
+                                profile, their ratings, and any other
                                 number of variables that are important to that individual (shared
                                 values). Similarly, this platform allows landlords to search for
                                 potential renters as well as get an idea of what the renting climate
